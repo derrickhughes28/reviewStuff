ff3e9c7ab3b1e8df8dc85d45d71bc8264c266a8b
derrick <derrick.hughes28@gmail.com>
2020-10-20 16:46:12 -0400

    do the thing

---
+++ app.js
@@ -0,0 +1,30 @@
+var car = {
+    "make": "Honda",
+    "model": "Civic",
+    "cylinders": 4,
+    "trim": ["EX", "DX", "LX", "SI"]
+  };
+
+
+  var truck = {
+    "make": "Kei",
+    "model": "Truck",
+    "cylinders": 4,
+    "trim": ["RWD", "4WD",]
+  };
+  
+  
+  var motorcycle = {
+    "make": "Ducati",
+    "model": "Scrambler",
+    "cylinders": 4,
+    "trim": ["sport", "street",]
+  };
+
+
+  var carValue = car["make"];
+  console.log(car["make"]);
+  var carValue = truck["make"];
+  console.log(truck["make"]);
+  var carValue = motorcycle["make"];
+  console.log(motorcycle["make"]);
